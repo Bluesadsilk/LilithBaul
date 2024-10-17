0796a054c4c0b9efd5349002e411a3ce0882c762
BlueKorpse <haxansword@gmail.com>
2024-10-17 23:38:05 +0200

    Swagger Working

--- src/main/java/com/nomudev/docs/SwaggerConfig.java
+++ src/main/java/com/nomudev/docs/SwaggerConfig.java
@@ -1,23 +1,10 @@
 package com.nomudev.docs;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
 @Configuration
-@EnableSwagger2
+@OpenAPIDefinition(info = @Info(title = "API Documentation", version = "v1"))
 public class SwaggerConfig {
-
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.tu.paquete")) // Cambia a tu paquete base
-                .paths(PathSelectors.any())
-                .build();
-    }
 }
